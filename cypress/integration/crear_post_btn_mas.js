80ea39f70c9e4cef2439d92f6757440ec5292222
Dario_herrera <d.herrerag@uniandes.edu.co>
2021-05-07 01:59:25 -0500

    Post_creation: se crean escenarios  para prueba de creacion de posts

---
+++ cypress/integration/crear_post_btn_mas.js
@@ -0,0 +1,62 @@
+/// <reference types="cypress"/>
+
+const url = Cypress.config('baseUrl') || "http://localhost:2368/ghost/#/signin"
+const delay = Cypress.env('delay') || 100
+const faker = require('faker');
+const config = require('../../posts-config.json')
+let loginURL = config.loginURL;
+let userInputID = config.values.userInputID;
+let passwordInputID = config.values.passwordInputID;
+let loginButtonID =  config.values.loginButtonID;
+let userNameLogin = config .values.userName;
+let passwordLogin = config.values.password; 
+
+function fillOutInput(text, idSearch){
+	const item = cy.get(idSearch).first()
+    item.type(text, {force: true});  
+}
+
+function clickButton(search, index ){
+	 cy.get(search).then($button => {
+        var buttonLink = $button.get(index);
+        if(!Cypress.dom.isHidden(buttonLink)) {
+           cy.wrap(buttonLink).click({force: true});	
+        } 
+    });
+}
+
+describe('Post E2E', () => {
+    beforeEach(() => {
+      cy.visit(url)
+      cy.wait(delay);
+    })
+
+    it('Ingresar con credenciales validos y crear un post con titulo haciendo uso de boton "+"', () => {
+        fillOutInput(userNameLogin, `[id^=${userInputID}]`);
+        fillOutInput(passwordLogin, `[id^=${passwordInputID}]`);
+        clickButton(`[id^=${loginButtonID}]`, 0);
+        cy.get('.gh-user-email').should('have.text', userNameLogin);
+        cy.wait(delay);
+
+
+        cy.get('.gh-nav-manage .gh-secondary-action').click();
+        cy.get('.gh-editor-header').contains('New');
+        cy.wait(delay);
+
+        let postTitle = faker.lorem.sentence();
+
+        cy.get('.gh-editor-title').type(postTitle);
+        cy.get('.gh-editor-header').contains("Posts");
+        cy.get('.koenig-editor__editor').click();
+        cy.go('back');
+        cy.wait(delay);
+        cy.get('#ember136').click()
+        cy.get('.gh-canvas-title').should('have.text', 'Posts');
+        cy.wait(delay);
+
+        cy.get('ol h3:first').should('have.text', `\n        ${postTitle}\n    `);
+        cy.wait(delay);
+
+                
+    })
+})
