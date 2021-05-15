0255fae0400e860158db3ddd259de0d0aa33b6ba
Alejito29 <W.gonzalezg@uniandes.edu.co>
2021-05-15 11:42:22 -0500

    Update the code to take screenshot

--- cypress/page-objects/constant-page.js
+++ cypress/page-objects/constant-page.js
@@ -1,8 +1,8 @@
 export class Constant_PAGES {
-    static USER = 'prueba@gmailcom';
+    static USER = 'prueba@gmail.com';
     static PASSWORD = '123456789*';
     static BASE_URL = 'http://localhost:2369/'
-    static DELAY = 600;
+    static DELAY = 800;
     static NEW_TITLE= 'Nuevo titulo';
     static NEW_DESCRIPTION= 'Nueva Descripcion';
     static NEW_LANGUAJE= 'es';
