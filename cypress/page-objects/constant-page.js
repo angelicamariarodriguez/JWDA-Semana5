8a27bac178a1c25df1ec06e17fc670f4d6b102d7
Alejito29 <W.gonzalezg@uniandes.edu.co>
2021-05-15 11:09:19 -0500

    Added the screen in manage pages

--- cypress/page-objects/constant-page.js
+++ cypress/page-objects/constant-page.js
@@ -1,9 +1,9 @@
 export class Constant_PAGES {
 
-    static USER = 'rodrigueztorresnancy468@gmail.com';
-    static PASSWORD = 'M4r14t0rr3s*';
+    static USER = 'prueba@gmail.com';
+    static PASSWORD = '123456789*';
     static BASE_URL = 'http://localhost:2368/'
-    static DELAY = 300;
+    static DELAY = 600;
     static NEW_TITLE= 'Nuevo titulo';
     static NEW_DESCRIPTION= 'Nueva Descripcion';
     static NEW_LANGUAJE= 'es';
